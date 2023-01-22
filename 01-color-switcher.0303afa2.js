c06b2733b75bfa74658f09a38debad6e348f238d
varewka7 <116559703+varewka7@users.noreply.github.com>
2023-01-22 16:05:33 +0000

    Deploying to gh-pages from @ varewka7/goit-js-hw-09@19ebada62ad747bac926264a7cd3419dca6e07f6 🚀

---
+++ 01-color-switcher.0303afa2.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")};const e={intervalId:null,isActive:!1,start(){this.isActive||(this.isActive=!0,this.intervalId=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3))},stop(){clearInterval(this.intervalId),this.isActive=!1}};t.startBtn.addEventListener("click",(()=>{e.start()})),t.stopBtn.addEventListener("click",(()=>{e.stop()}));
+//# sourceMappingURL=01-color-switcher.0303afa2.js.map
